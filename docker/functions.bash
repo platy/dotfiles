9889da9b99bf2178b3169829ff18934fa95538d0
Mike Bush <mpbush@gmail.com>
2016-10-11 14:04:54 +0200

    Move docker build to the startup function as the docker app needs to be authorised ans running to enable it - so it wouldn't be available during install

--- docker/functions.bash
+++ docker/functions.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 trustyd() {
+  docker build -f $DOTFILES/docker/trusty/Dockerfile -t trusty $DOTFILES
   docker run -it --rm trusty
 }
 
--- docker/homebrew.install.sh
+++ docker/homebrew.install.sh
@@ -4,5 +4,3 @@ set -e
 
 brew install Caskroom/versions/docker-beta
 
-docker build -f $DOTFILES/docker/trusty/Dockerfile -t trusty $DOTFILES
-
