b10c90221329b021db1f23f122f0c510a31f1126
Mike Bush <mpbush@gmail.com>
2016-12-01 12:58:26 +0100

    Add Vertex centos docker image

--- docker/functions.bash
+++ docker/functions.bash
@@ -5,7 +5,16 @@ build_trustyd() {
 }
 
 trustyd() {
-  build_trustyd
+  build_trustyd && \
   docker run -it --rm -v $HOME:/home/host -v $DOTFILES:/root/.dotfiles trusty
+}
+
+build_vertex_centos() {
+  docker build -f $DOTFILES/docker/vertex-centos/Dockerfile -t vertex-centos $DOTFILES
+}
+
+vertex-centos() {
+  build_vertex_centos && \
+  docker run -it --rm -v $HOME:/home/host -v $DOTFILES:/root/.dotfiles vertex-centos
 }
 
