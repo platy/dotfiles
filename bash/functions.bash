7b32d510b98b214390d80828f4ccce5a2cf039f3
Mike Bush <mpbush@gmail.com>
2017-06-09 19:00:57 +0200

    Add a few aliases

--- bash/functions.bash
+++ bash/functions.bash
@@ -3,3 +3,6 @@
 myip () {
   dig +short myip.opendns.com @resolver1.opendns.com
 }
+
+alias :q="echo NOT IN VIM!!!"
+
---
+++ docker/alias.bash
@@ -0,0 +1,4 @@
+alias dk=docker
+alias dm=docker-machine
+alias dc=docker-compose
+
