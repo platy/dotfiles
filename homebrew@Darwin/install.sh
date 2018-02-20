3463cd3445aa2b51f1fe6546e99aa342419473ea
Mike Bush <mpbush@gmail.com>
2018-02-20 07:52:06 +0100

    Add more intro and tangle a single Brewfile

--- homebrew@Darwin/install.sh
+++ homebrew@Darwin/install.sh
@@ -8,20 +8,16 @@ if test ! $(which brew)
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-cd "$(dirname $0)"/..
-
 # find the homebrew bundles and run them all
-echo "Installing brew bundles"
-cat $(find . -name *.brew) | \
-  brew bundle --file=-
+echo "Installing brew bundle"
+brew bundle --file=gen/Brewfile
 
 # find the homebrew dependent installers and run them iteratively
-find . -name homebrew.install.sh \
+find $DOTFILES -name homebrew.install.sh \
   | while read installer; do
       echo "installing ${installer}"
       sh -c "${installer}"
       echo "installed ${installer}"
   done
-
 
 exit 0
