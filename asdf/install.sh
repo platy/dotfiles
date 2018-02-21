7df22ee8bab19a31d0a94babc31bff66f9737dd7
Mike Bush <mpbush@gmail.com>
2018-02-21 11:06:39 +0100

    Concatenate installers instead of using separate files

--- asdf/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-ASDF_DIR=$HOME/.asdf
-ASDF_BRANCH=v0.4.1
-if [ ! -d $ASDF_DIR ]; then # new install
-  git clone https://github.com/asdf-vm/asdf.git --branch $ASDF_BRANCH $ASDF_DIR && cd $ASDF_DIR && git checkout $ASDF_BRANCH
-else
-  cd "$ASDF_DIR" && git pull origin $ASDF_BRANCH
-fi
--- emacs/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -d ~/.emacs.d ]; then # new install
-  git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-else
-  cd ~/.emacs.d && git pull
-fi
